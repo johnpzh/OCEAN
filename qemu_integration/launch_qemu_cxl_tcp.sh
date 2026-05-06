37243d16b1443c94245b07dd9adbc81d97dcbfe2
johnpzh <hi.pengzhen@gmail.com>
2026-05-06 01:52:37 +0000

    Updated launch qemu script; larger memory for ExaChem

--- qemu_integration/launch_qemu_cxl_tcp.sh
+++ qemu_integration/launch_qemu_cxl_tcp.sh
@@ -9,17 +9,23 @@ DISK_IMAGE=${DISK_IMAGE:-plucky-server-cloudimg-amd64.img}
 
 # Enable SHM mode with lock-free coherency
 export CXL_TRANSPORT_MODE=shm
-export CXL_HOST_ID=1
+export CXL_HOST_ID=0
+
+export CXL_MEMSIM_HOST=192.168.100.1  # bridge IP
+export CXL_MEMSIM_PORT=9999
+export CXL_TRANSPORT_MODE=tcp
+export CXL_HOST_ID=0
+
 $QEMU_BINARY \
     --enable-kvm -cpu qemu64,+xsave,+rdtscp,+avx,+avx2,+sse4.1,+sse4.2,+avx512f,+avx512dq,+avx512ifma,+avx512cd,+avx512bw,+avx512vl,+avx512vbmi,+clflushopt  \
-    -m 32G,maxmem=64G,slots=8 \
+    -m 16G,maxmem=32G,slots=8 \
     -smp 4 \
     -M q35,cxl=on \
     -kernel ./bzImage \
     -append "root=/dev/sda rw console=ttyS0,115200 nokaslr" \
-    -drive file=./qemu1.img,index=0,media=disk,format=raw \
-    -netdev tap,id=net0,ifname=tap1,script=no,downscript=no \
-    -device virtio-net-pci,netdev=net0,mac=52:54:00:00:00:02 \
+    -drive file=./qemu.img,index=0,media=disk,format=raw \
+    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+    -device virtio-net-pci,netdev=net0,mac=52:54:00:00:00:01 \
     -fsdev local,security_model=none,id=fsdev0,path=/dev/shm \
     -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshm,bus=pcie.0 \
     -device pxb-cxl,bus_nr=12,bus=pcie.0,id=cxl.1 \
@@ -28,8 +34,8 @@ $QEMU_BINARY \
     -device cxl-type3,bus=root_port13,persistent-memdev=cxl-mem1,lsa=cxl-lsa1,id=cxl-pmem0,sn=0x1 \
     -device cxl-type1,bus=root_port14,size=1G,cache-size=64M \
     -device virtio-cxl-accel-pci,bus=pcie.0 \
-    -object memory-backend-file,id=cxl-mem1,share=on,mem-path=/dev/shm/cxlmemsim_pgas,size=32G \
-    -object memory-backend-file,id=cxl-lsa1,share=on,mem-path=/dev/shm/lsa1.raw,size=1G \
+    -object memory-backend-file,id=cxl-mem1,share=on,mem-path=/dev/shm/cxlmemsim_dist_node0,size=4G \
+    -object memory-backend-file,id=cxl-lsa1,share=on,mem-path=/dev/shm/lsa0.raw,size=1G \
     -M cxl-fmw.0.targets.0=cxl.1,cxl-fmw.0.size=4G \
     -fsdev local,id=shared,path=/home/exouser/local,security_model=mapped \
     -device virtio-9p-pci,fsdev=shared,mount_tag=hostshare,bus=pcie.0 \
